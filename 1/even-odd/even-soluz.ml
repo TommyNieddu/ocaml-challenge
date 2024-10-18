9dc2fd9d867535a2bf1417d834e41e20f6612db6
TommyNieddu <thomasnieddu30@gmail.com>
2024-10-18 18:30:57 +0200

    even-soluz

--- 1/even-odd/even-soluz.ml
+++ 1/even-odd/even-soluz.ml
@@ -1,1 +1,5 @@
-let even a b = if (a >= 0 && a <= 5) && a mod b = 0 then 1 else if (b >= 0 && b <= 5) && a mod b != 0 then -1 else 0;;
+let even a b =
+  if b = 0 then 0  
+  else if (a >= 0 && a <= 5) && a mod b = 0 then 1
+  else if (b >= 0 && b <= 5) && a mod b <> 0 then -1
+  else 0
